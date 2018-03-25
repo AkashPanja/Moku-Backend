0993e851c7bbe7c243f568d5e2c7b5b4639fb1da
Akash Panja <akash.panja@gmail.com>
2018-03-25 21:46:52 +0530

    Database Add On

--- app.js
+++ app.js
@@ -11,11 +11,23 @@ app.use(basicAuth({
 app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
 
 app.get('/', function(req, res){
-res.send("Hello World!");
+ res.send("Hello World!");
 });
 
 app.post('/', (request, response) => {
   response.json(request.body);
+ var mysql = require('mysql');
+
+ var con = mysql.createConnection({
+   host: "localhost",
+   user: "blaze",
+   password: "Node2021"
+ });
+
+ con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+ });
 });
 
 app.listen(3000);
