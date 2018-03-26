44394483c45075fb32d2398f3fdc61e062e96fa1
Akash Panja <akash.panja@gmail.com>
2018-03-26 15:06:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,16 +24,9 @@ app.get('/', function(req, res){
 
 app.post('/', (request, response) => {
 
-   response.json(request.body);
-
 con.connect(function(err) {
   if (err) throw err;
-  response.send("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    response.send("Table created");
-  });
+  console.log("Connected!");
 });
  
 });
