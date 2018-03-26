2c360a2f3331f595e9e5d46b82f7e9cb0974b7fb
Akash Panja <akash.panja@gmail.com>
2018-03-26 17:58:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 //Require
 var express=require('express');
-var mysql=require('mysql');
-var bodyParser=require('body-parser');
-
-
-app.use(bodyParser.json());
 
 //Objects
 var app=express();
-var conn=mysql.createConnection({
-   host:"localhost",
-   user:"blaze",
-   password:"Node2021",
-   database:"moku"
-});
 
 //Methods
 app.get('/',function(req,res){
-   res.end("hello world");
-});
-
-app.post('/', (request, response) => {
-  response.json(request.body);
+   res.send("Hello World");
 });
 
 //Port
