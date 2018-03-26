6bda1efcccaa94553271f2d5d89ed127e4e229b2
Akash Panja <akash.panja@gmail.com>
2018-03-26 17:07:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var express = require('express');
-var mysql = require('mysql');
 var bodyParser = require('body-parser');
-var con = mysql.createConnection({
-   host: "localhost",
-   user: "blaze",
-   password: "Node2021",
-   database: "moku"
- });
-
 var app = express();
 
-app.post('/',function(req,res){
-  res.end("Hello World");
+const basicAuth = require('express-basic-auth');
+ 
+app.use(basicAuth({
+    users: { 'blaze': 'Node2021' }
+}));
+
+app.use(bodyParser.json()); // add a middleware (so that express can parse request.body's json)
+
+app.get('/', function(req, res){
+res.send("Hello World!");
 });
 
+app.post('/', (request, response) => {
+  response.json(request.body);
+});
 
 app.listen(3000);
