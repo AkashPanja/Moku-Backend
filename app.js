f28dcb26e5fb5ead6a7e4de5efb5116e56467bf7
Akash Panja <akash.panja@gmail.com>
2018-03-26 17:51:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,14 +22,7 @@ var conn=mysql.createConnection({
 
 //Methods
 app.get('/',function(req,res){
-   conn.connect(function(err){
-   if(err) throw err;
-      var sql="CREATE TABLE ALBUM ( `ALBUM_ID` VARCHAR(30) NOT NULL , `ALBUM_NAME` VARCHAR(30) NOT NULL , `ALBUM_ARTIST` VARCHAR(30) NOT NULL , `ALBUM_ART` VARCHAR(30) NOT NULL )";
-      conn.query(sql,function(err,result){
-         if(err) throw err;
-         res.send("Table Created");
-      });
-   });
+   res.end("hello world");
 });
 
 app.post('/', (request, response) => {
