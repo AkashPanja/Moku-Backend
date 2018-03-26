cff985bd57e8e1e57c74113d41d32553a9832cba
Akash Panja <akash.panja@gmail.com>
2018-03-26 23:49:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/ALBUM',function(req,res){
    var jObj=JSON.parse(req.body);
    conn.connect(function(err){
    if(err) throw err;
-      var sql="SELECT * FROM ALBUM_INDEX LIMIT "+jObj.body.OFFSET+","+jObj.body.LIMIT;
+      var sql="SELECT * FROM ALBUM_INDEX LIMIT "+jObj.OFFSET+","+jObj.LIMIT;
       conn.query(sql,function(err,result,fields){
       if(err) throw err;
          res.json(result);
